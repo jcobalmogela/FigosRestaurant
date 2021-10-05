eccbcbfce68766c2c66655bd7e5c6f1e4ee6d40a
jcobalmogela <62285327+jcobalmogela@users.noreply.github.com>
2021-10-05 13:15:29 +0800

    new updatge

--- signIn.js
+++ signIn.js
@@ -1,5 +1,13 @@
 const btnSignIn = document.querySelectorAll('.btnSignIn');
 const modalSignIn = document.querySelector('.signInValidation');
+const email = document.getElementById('email');
+const pass = document.getElementById('password');
+const form = document.getElementById('form');
+const errorMessage = document.getElementById('error');
+const shop = document.querySelector(".shop-icon");
+const loginBtn = document.querySelector("#login");
+console.log(shop);
+
 
 
 btnSignIn.forEach(button => {
@@ -8,3 +16,29 @@ btnSignIn.forEach(button => {
  })
 });
 
+form.addEventListener('submit', (e) => {
+   let messages = []
+   let state = 0;
+console.log();
+   if(email.value === '' || email.value == null){
+    messages.push('Email is required');
+   }else{
+    e.preventDefault();
+   }
+   if(password.value.length <= 8){
+    messages.push('Password must be longer than 8 characters');
+   }else{
+    e.preventDefault();
+    location.href = "index.html";
+   
+   }
+   if(messages.length > 0){
+    e.preventDefault();
+     errorMessage.innerText = messages.join(', ');
+   }
+   if(state === 1){
+        shop.classList.remove('none');
+   }
+
+
+})
